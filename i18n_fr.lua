781faccee7a741493da59130f1c86bff1c744bfe
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,6 @@
 return {
     ["You won! W:%1 L:%2"]                = { fr = "Vous avez gagné ! V:%1 D:%2" },
     ["Wrong: %1/%2  W:%3 L:%4"]           = { fr = "Faux : %1/%2  V:%3 D:%4" },
+    ["Classic word-guessing game. Reveal the word before the hangman is drawn."] = { fr = "Jeu classique de devinette de mots. Révélez le mot avant que le pendu ne soit dessiné." },
+    ["Hangman"]                           = { fr = "Pendu" },
 }
