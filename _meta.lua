da5fd1e881c5c13ea5b84c735e35301cb7f5b363
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:50:33 +0200

    chore: bump version to 1.1.12

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Hangman"),
     description = _("Classic word-guessing game. Reveal the word before the hangman is drawn."),
-    version     = "1.1.11",
+    version     = "1.1.12",
 }
