9b5d0be66b3a9a584e211a71122a5f366733e059
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:58:54 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,6 +1,6 @@
 return {
-    ["You won! W:%1 L:%2"]                = { fr = "Vous avez gagné ! V:%1 D:%2" },
-    ["Wrong: %1/%2  W:%3 L:%4"]           = { fr = "Faux : %1/%2  V:%3 D:%4" },
-    ["Classic word-guessing game. Reveal the word before the hangman is drawn."] = { fr = "Jeu classique de devinette de mots. Révélez le mot avant que le pendu ne soit dessiné." },
-    ["Hangman"]                           = { fr = "Pendu" },
+    ["You won! W:%1 L:%2"]                = { fr = "Vous avez gagné ! V:%1 D:%2", es = "¡Has ganado! V:%1 D:%2", de = "Du hast gewonnen! S:%1 N:%2" },
+    ["Wrong: %1/%2  W:%3 L:%4"]           = { fr = "Faux : %1/%2  V:%3 D:%4", es = "Fallos: %1/%2  V:%3 D:%4", de = "Falsch: %1/%2  S:%3 N:%4" },
+    ["Classic word-guessing game. Reveal the word before the hangman is drawn."] = { fr = "Jeu classique de devinette de mots. Révélez le mot avant que le pendu ne soit dessiné.", es = "Juego clásico de adivinar palabras. Descubre la palabra antes de que se complete el dibujo del ahorcado.", de = "Klassisches Wortratespiel. Errate das Wort, bevor das Galgenmännchen fertig gezeichnet ist." },
+    ["Hangman"]                           = { fr = "Pendu", es = "Ahorcado", de = "Galgenmännchen" },
 }
